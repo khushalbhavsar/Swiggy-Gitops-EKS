9d35081c67735dc620b914ba8bcc6ce9520c670d
khushalbhavsar <khushalbhavsar41@gmail.com>
2026-02-26 13:01:32 +0530

    Commit

--- infrastructure/modules/ec2-jumphost/providers.tf
+++ infrastructure/modules/ec2-jumphost/providers.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "swiggy111"
+    bucket = "swiggy-gitops-tfstate-843998948464"
     key    = "ec2/terraform.tfstate"
     region = "us-east-1"
   }
--- infrastructure/modules/ec2-jumphost/variables.tf
+++ infrastructure/modules/ec2-jumphost/variables.tf
@@ -64,7 +64,7 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0150ccaf51ab55a51" // Replace with the latest AMI ID for your region
+  default     = "ami-0f3caa1cf4417e51b" // Replace with the latest AMI ID for your region
 }
 
 variable "instance_type" {
@@ -76,7 +76,7 @@ variable "instance_type" {
 variable "key_name" {
   description = "EC2 keypair"
   type        = string
-  default     = "us-east-1"   // Replace with the keypair
+  default     = "awsKey"   // Replace with the keypair
 }
 
 variable "instance_name" {
